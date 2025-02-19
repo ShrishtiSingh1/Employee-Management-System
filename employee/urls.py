44d3753d16e8f17b2db5f32dad753bd0c00ff3e8
ShrishtiSingh1 <144584216+ShrishtiSingh1@users.noreply.github.com>
2025-02-19 17:10:09 +0530

    urls.py

--- employee/urls.py
+++ employee/urls.py
@@ -1,12 +1,13 @@
 
 from django.urls import path # type: ignore
-from .views import get_employees, create_employee, update_employee, delete_employee, home
+from .views import get_employees, create_employee, update_employee, delete_employee, home, employeeInfo
 
 urlpatterns = [
     path('employees/', get_employees, name='get_employees'),                                    #get
     path('employees/create/', create_employee, name='create_employee'),                         #post
     path('employees/<int:pk>/update/', update_employee, name='update_employee'),                #put
     path('employees/<int:pk>/delete/', delete_employee, name='delete_employee'),                #delete
-    path('', home, name="home")                                                                 #home
+    path('', home, name="home"),                                                                 #home
+    path('employee/info/', employeeInfo, name="employeeInfo"),                                 
 ]
 

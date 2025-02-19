930034e60a5aecd160a75c4c24037578a76439a0
ShrishtiSingh1 <144584216+ShrishtiSingh1@users.noreply.github.com>
2025-02-19 11:33:51 +0530

    settings.py

---
+++ employee_mgmnt_system/settings.py
@@ -0,0 +1,23 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'employee', 
+    'rest_framework', 
+]
+
+DATABASES = {
+    'default':{    
+                    'AUTOCOMMIT': True,
+                    'ENGINE': 'django.db.backends.postgresql',
+                    'NAME':"postgres",
+                    'USER': "postgres",
+                    'PASSWORD':"postgres",
+                    'HOST':"localhost",
+                    'PORT':"5433",
+                 },    
+ 
+}
